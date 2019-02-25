71e8f7bb94b4116d4500eae9e7730788f3773f9c
Eliot Cowley <eliot93@live.com>
2019-02-24 23:05:10 -0800

    Added button with click event

---
+++ EliotsPieShop/EliotsPieShop/EliotsPieShop/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EliotsPieShop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DemoButton_Clicked(object sender, EventArgs e)
+        {
+            DemoButton.Text = "I was clicked";
+        }
+    }
+}
